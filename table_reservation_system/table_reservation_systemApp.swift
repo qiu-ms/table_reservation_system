a2b60f730b495073fab9339ed429e9bea703eb8e
qiu-ms <qiumenseng@gmail.com>
2023-12-03 17:17:41 +0100

    Initial Commit

---
+++ table_reservation_system/table_reservation_systemApp.swift
@@ -0,0 +1,17 @@
+//
+//  table_reservation_systemApp.swift
+//  table_reservation_system
+//
+//  Created by Qiu, Men Seng on 03.12.23.
+//
+
+import SwiftUI
+
+@main
+struct table_reservation_systemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
